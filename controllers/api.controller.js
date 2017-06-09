b49953b8a8e7e551365c5102a131bfb2fc996f95
CodingKaiser <falkonoe@live.com>
2017-06-08 23:07:03 -0700

    Updated model to handle get, post, and del

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -3,13 +3,31 @@ var Article = require("../models/articles.js")
 
 // Create all our routes and set up logic within those routes where required.
 module.exports.getArticles = (req, res) => {
-
+  Article.find({}).limit(5).sort({date: -1}).exec((err, found) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json(found)
+    }
+  })
 }
 
 module.exports.postArticle = (req, res) => {
-
+  Article.create(req.body, (err, saved) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json(saved)
+    }
+  })
 }
 
 module.exports.delArticle = (req, res) => {
-
+  Article.remove(req.body, (err, removed) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json(removed)
+    }
+  })
 }
